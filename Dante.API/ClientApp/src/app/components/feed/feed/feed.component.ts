043b7bb69496aacafda352d6cdc1caf012769e10
Dante Ruiz <dante.ruiz@dxc.com>
2023-12-01 01:50:01 +0800

    feed ui initial

--- Dante.API/ClientApp/src/app/components/feed/feed/feed.component.ts
+++ Dante.API/ClientApp/src/app/components/feed/feed/feed.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tweet } from 'src/app/models/tweet.model';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,8 +9,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
+  tweets: Tweet[] = [
+    { user: 'Dante', content: 'This is a tweet' },
+    { user: 'Sasha', content: 'This is a tweet again' },
+    { user: 'Reiner', content: 'This is a tweet again twice' }
+  ]
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, private router: Router) { }
 
   logout() {
     this.authService.storeToken('');
---
+++ Dante.API/ClientApp/src/app/models/tweet.model.ts
@@ -0,0 +1,4 @@
+export interface Tweet {
+    user: string;
+    content: string;
+}
